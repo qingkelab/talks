e01f7872f0c1d1d7d7bfe85fdc5971ee41a5dc2e
King-Key <guo_wang_113@163.com>
2024-03-12 18:29:33 +0800

    Site updated: 2024-03-12 18:29:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/19/JD/alijd/","2023/12/02/AIGC/AnimateAnyone/","2024/03/11/LLM/2024-03-11/","2024/02/04/LLM/AI Agent/20240204/","2024/02/26/zhibo/Panel/20240302/"];function toRandomPost(){
+var posts=["2023/12/02/AIGC/AnimateAnyone/","2024/01/19/JD/alijd/","2024/03/11/LLM/2024-03-11/","2024/02/04/LLM/AI Agent/20240204/","2024/02/26/zhibo/Panel/20240302/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
