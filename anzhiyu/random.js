77ef275716638d76d25a7501dfb52dd6938e48fc
King-Key <guo_wang_113@163.com>
2024-03-12 15:53:43 +0800

    Site updated: 2024-03-12 15:53:43

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2023/12/02/AIGC/AnimateAnyone/","2024/02/04/AI Agent/20240204/","2024/01/19/JD/alijd/","2024/02/26/zhibo/Panel/20240302/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
